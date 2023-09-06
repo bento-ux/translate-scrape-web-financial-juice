da0bcbfe703e612af4c1a1edc7c08930a12ea011
bento-ux <58581260+bento-ux@users.noreply.github.com>
2023-09-06 14:43:15 +0700

    Delete translate.js

--- translate.js
+++
@@ -1,7 +0,0 @@
-const translate = require('@iamtraction/google-translate');
-
-translate('mergo tresno', { to: 'en' }).then(res => {
-    console.log(res.text); // OUTPUT: You are amazing!
-  }).catch(err => {
-    console.error(err);
-  });
